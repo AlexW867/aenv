1836fefa0a348817627a4e3fb9c5273ac5889bfd
AlexW <yen867@gmail.com>
2025-09-30 01:08:33 +0800

    add deinstall.sh

---
+++ deinstall.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm -fr \
+    ~/.dotfiles \
+    ~/.oh-my-zsh \
+    ~/.tmux \
+    ~/.config/nvim \
+    ~/.local/state/nvim \
+    ~/.local/share/nvim
+
+rm -f ~/.tmux.conf
+rm -f ~/.zshrc
+rm -f ~/.tmux.conf
+rm -f ~/.gitconfig
+rm -f ~/.config/starship.toml
